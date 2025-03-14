809e3a5d252dc49840b615d77bd95985c655c507
Léonard <Leeooonaard@gmail.com>
2025-03-14 12:06:32 +0800

    restore

---
+++ Examples/Get-ExtendedAttribute.ps1
@@ -0,0 +1,42 @@
+﻿function Get-IndividualExtendedAttribute
+{
+    param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'File')]
+        [System.IO.FileInfo]
+        $File,
+
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'Directory')]
+        [System.IO.DirectoryInfo]
+        $Directory
+    )
+
+    try
+    {
+        if($PSCmdlet.ParameterSetName -eq 'Directory')
+        {
+            $FileMode = $FILE_DIRECTORY_FILE
+            $FullName = $Directory.FullName
+        }
+        else
+        {
+            $FileMode = $FILE_NON_DIRECTORY_FILE
+            $FullName = $File.FullName
+        }
+    
+        $FileHandle = NtOpenFile -FilePath $FullName -AccessMask ($READ_CONTROL -bor $FILE_READ_EA) -ShareAccess ([System.IO.FileShare]::Delete -bor [System.IO.FileShare]::ReadWrite) -OpenOptions ($FILE_OPEN_FOR_BACKUP_INTENT -bor $FILE_RANDOM_ACCESS -bor $FileMode)
+
+        $EA = ZwQueryEaFile -FileHandle $FileHandle
+            
+        if($EA -ne $null)
+        {
+            $EA | Add-Member -MemberType NoteProperty -Name FilePath -Value $FilePath
+
+            Write-Output $EA
+        }
+    }
+    catch
+    {
+
+    }
+}
---
+++ Examples/Set-ExtendedAttribute.ps1
@@ -0,0 +1,40 @@
+﻿function Set-ExtendedAttribute
+{
+    <#
+    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .NOTES
+
+    .EXAMPLE
+    #>
+
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $FilePath,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Name,
+
+        [Parameter(Mandatory = $true)]
+        [byte[]]
+        $Value
+    )
+
+    if(Test-Path -Path $FilePath -PathType Container)
+    {
+        $FileMode = $FILE_DIRECTORY_FILE
+    }
+    else
+    {
+        $FileMode = $FILE_NON_DIRECTORY_FILE
+    }
+
+    $FileHandle = NtOpenFile -FilePath $FilePath -AccessMask ($FILE_GENERIC_WRITE -bor $FILE_WRITE_EA) -ShareAccess ([System.IO.FileShare]::Read -bor [System.IO.FileShare]::Write) -OpenOptions ($FILE_RANDOM_ACCESS -bor $FileMode)
+
+    ZwSetEaFile -FileHandle $FileHandle -Name $Name -Value $Value
+}
