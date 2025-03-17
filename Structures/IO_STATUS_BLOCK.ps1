3f3ffbb92cf3c1d11d48aed6284013d795448dfe
Léonard <Leeooonaard@gmail.com>
2025-03-17 23:18:58 +0800

    Fix IO_STATUS_BLOCK struct.
    This nearly crashed powershell 7.5 when using NtQueryEaFile.
    Here's a documentation of the struct:
    https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/ns-wdm-_io_status_block

--- Structures/IO_STATUS_BLOCK.ps1
+++ Structures/IO_STATUS_BLOCK.ps1
@@ -1,4 +1,4 @@
 $IO_STATUS_BLOCK = struct $Module IO_STATUS_BLOCK @{
-    Status  = field 0 Int64  -Offset 0
-    Pointer = field 1 IntPtr -Offset 0
-} -ExplicitLayout
+    Status  = field 0 UInt32
+    Pointer = field 1 IntPtr
+}
